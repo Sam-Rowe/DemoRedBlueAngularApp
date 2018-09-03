0fec4e10184669464c3012037e10cab93b502ca6
Samuel Rowe <samuel.rowe@microsoft.com>
2018-09-03 17:13:02 +0100

    Inital commit of the code and infrastructure.

---
+++ code/demo-red-blue/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RedComponent } from './red/red.component';
+import { BlueComponent } from './blue/blue.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path:'red',
+        component: RedComponent
+      },
+      {
+        path: 'blue',
+        component: BlueComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
