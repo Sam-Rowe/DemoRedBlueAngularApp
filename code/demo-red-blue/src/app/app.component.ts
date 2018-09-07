cdcf579317c69c64519719594955ac1982dbc702
Samuel Rowe <samuel.rowe@microsoft.com>
2018-09-07 09:24:15 +0100

    Simple demo change

--- code/demo-red-blue/src/app/app.component.ts
+++ code/demo-red-blue/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   <h1> Angular CI Example! </h1>
   <nav>
     <a routerLink="/red">RED</a>
+    </hr>
     <a routerLink="/blue">BLUE</a>
   </nav>
   <router-outlet></router-outlet>
